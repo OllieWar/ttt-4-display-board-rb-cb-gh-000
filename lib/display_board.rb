780f421441b4b4665e341978160094adc1337d94
Oliver Warway <olliewarway@gmail.com>
2017-11-21 14:56:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 pos = [" "," "," "," ","X"," "," "," "," "]
-def display_board(pos)
+def display_board(board)
   puts " #{pos[0]} | #{pos[1]} | #{pos[2]} "
   puts "-----------"
   puts " #{pos[3]} | #{pos[4]} | #{pos[5]} "
